ee452bfe2f42417de9631fddf588ee3d4ee1654c
unknown <ahmedirheem2003@gmail.com>
2023-03-29 16:20:44 +0300

    Fixing Error Relates #18

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,24 +4,22 @@ const homePage = (req, res) => {
   res.sendFile(path.join(__dirname, '..', '..', 'public', 'index.html'));
 };
 
-const searchJob = (req, res) => {
+const getJobs = (req, res) => {
   const { title } = req.body;
   const { location } = req.body;
 
-  // const url = `https://jsearch.p.rapidapi.com/search?query=${title}&page=1&num_pages=1`;
-  const url2 = 'https://jsearch.p.rapidapi.com/search?query=Python%20developer%20in%20Texas%2C%20USA&page=1&num_pages=1'
-  
+  const url2 = `https://jsearch.p.rapidapi.com/search?query=${title}%20in%20${location}&page=1&num_pages=1`;
+
   const options = {
     method: 'GET',
     headers: {
       'X-RapidAPI-Key': '1e4529541amshe14048ca9b323f2p1badfbjsnb3e2336b9718',
-      'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
-    }
+      'X-RapidAPI-Host': 'jsearch.p.rapidapi.com',
+    },
   };
   fetch(url2, options)
     .then((result) => result.json())
     .then((result) => res.send(result))
     .catch((error) => console.log(error));
 };
-
-module.exports = { homePage, searchJob };
+module.exports = { homePage, getJobs };
